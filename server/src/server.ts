f2d35ab843fbcf5c97ac6299324655bd30541102
Black Magic <dlyoungworking@gmail.com>
2025-01-13 23:40:04 -0800

    up 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 // ✅ Fix: Correct path for production build files
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
+    app.get('*', (_req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
 }
