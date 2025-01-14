82ea7896bbe579276d5fbd4347ac81f530865969
Black Magic <dlyoungworking@gmail.com>
2025-01-13 23:08:10 -0800

    up

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,51 @@
+
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url'; // Import for __dirname workaround
+import db from './config/connection.js';
+import routes from './routes/index.js';
+
+// Define __dirname in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
@@ -19,3 +67,7 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+
+*/
